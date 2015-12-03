f78473c4dcaa07570228a71491b2318866abc94b
wesleyyc <wesley.chin0919@gmail.com>
2015-12-03 12:57:20 -0500

    Finish

--- MapDW/ViewController.swift
+++ MapDW/ViewController.swift
@@ -21,19 +21,20 @@ class ViewController: UIViewController, EILIndoorLocationManagerDelegate {
     let fixLat: Double = 42.4817457
     let fixLong: Double = -71.2149056
     
-    var lats = [Double] (count: 3, repeatedValue: 0)
-    var longs = [Double] (count: 3, repeatedValue: 0)
+    var lats = [Double] (count: 3, repeatedValue: 42.4817457)
+    var longs = [Double] (count: 3, repeatedValue: -71.2149056)
     
-    var markers = [GMSMarker](count: 3, repeatedValue: GMSMarker())
+    var markers = [GMSMarker]()
     
-    let updateFreq: Double = 0.1
+    let updateFreq: Double = 4
     
     var xP=0.0
     var yP=0.0
     
+    var mapView:GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let query = PFQuery(className:"TestObject")
         query.whereKey("uuid", equalTo:uuid)
@@ -67,16 +68,19 @@ class ViewController: UIViewController, EILIndoorLocationManagerDelegate {
         // draw the map
         let camera = GMSCameraPosition.cameraWithLatitude(fixLat,
             longitude: fixLong, zoom: 19.5)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
         mapView.animateToBearing(63)
         self.view = mapView
         
         // draw the personal markers
-        lats[0] = fixLat + xP/1000
-        longs[0] = fixLong + yP/1000
+        markers.append(GMSMarker())
+        markers.append(GMSMarker())
+        markers.append(GMSMarker())
         markers[0].icon = GMSMarker.markerImageWithColor(UIColor.blueColor())
         markers[0].position = CLLocationCoordinate2DMake(lats[0], longs[0])
+        markers[0].title = "Me"
+        markers[0].snippet = "Shopper"
         markers[0].map = mapView
         
         
@@ -118,6 +122,44 @@ class ViewController: UIViewController, EILIndoorLocationManagerDelegate {
         lats[0] = fixLat + (yP-1)/8500
         longs[0] = fixLong + (xP-1)/8000
         markers[0].position = CLLocationCoordinate2DMake(lats[0], longs[0])
+        reloadOtherMarker()
+    }
+    
+    func reloadOtherMarker(){
+        let query = PFQuery(className:"TestObject")
+        query.whereKey("uuid", notEqualTo:self.uuid)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // Do something with the found objects
+                if let objects = objects {
+                    var count = 1
+                    for object in objects {
+                        let nXP = object["xP"] as! Double
+                        let nYP = object["yP"] as! Double
+                        self.lats[count] = self.fixLat + (nYP-1)/8500
+                        self.longs[count] = self.fixLong + (nXP-1)/8000
+                        self.markers[count].position = CLLocationCoordinate2DMake(self.lats[count], self.longs[count])
+                        self.markers[count].title = object.objectId
+                        self.markers[count].snippet = "Assistant"
+                        self.markers[count].map=self.mapView
+                        count++
+                        if(count>2){
+                            break;
+                        }
+
+                    }
+                    while(count<3){
+                        self.markers[count].map=nil
+                        count++
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
     }
     
     func updateXY(){
